f0fab14bffb38db05778e8ccbdd5cfb5de5a9fe5
Saran Jenjirawat <registbacktown@gmail.com>
2020-01-31 19:34:34 +0700

    Merge branch 'master' of https://github.com/aliceblock/longdo_map

--- android/src/main/kotlin/me/intception/longdo_map/LongdoMap.kt
+++ android/src/main/kotlin/me/intception/longdo_map/LongdoMap.kt
@@ -2,24 +2,27 @@ package me.intception.longdo_map
 
 import android.view.View
 import android.widget.Toast
+import androidx.core.app.ActivityCompat
 import com.longdo.api.Map
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.PluginRegistry
 import io.flutter.plugin.platform.PlatformView
 import com.longdo.api.MapGLSurfaceView
+import io.flutter.plugin.common.StandardMessageCodec
 
 class LongdoMap(private val registrar: PluginRegistry.Registrar, viewId: Int, args: Any?) : PlatformView, MethodChannel.MethodCallHandler {
     private val channel = MethodChannel(registrar.messenger(), "me.intception.longdo_map/$viewId")
-    private val mapView = getLongdoMap(registrar)
     private lateinit var longdoMap: Map
+    private val mapView = getLongdoMap(registrar)
 
     init {
         channel.setMethodCallHandler(this)
     }
 
-    private fun getLongdoMap(registrar: PluginRegistry.Registrar) =
-         MapGLSurfaceView(registrar.context()).also { it.setListener { longdoMap = it } }
+    private fun getLongdoMap(registrar: PluginRegistry.Registrar): MapGLSurfaceView {
+        return MapGLSurfaceView(registrar.context()).also { it.setListener { longdoMap = it } }
+    }
 
     override fun getView(): View {
         return mapView
@@ -30,9 +33,15 @@ class LongdoMap(private val registrar: PluginRegistry.Registrar, viewId: Int, ar
     }
 
     override fun onMethodCall(call: MethodCall, result: MethodChannel.Result) {
-        when (call.arguments) {
-            "doSomething" -> {
-                Toast.makeText(registrar.context(), "Do something", Toast.LENGTH_LONG)
+        when (call.method) {
+//            "goToCurrentLocation" -> {
+//                longdoMap.updateAndShowCurrentLocation()
+//            }
+            "zoomIn" -> {
+                longdoMap.zoomIn()
+            }
+            "zoomOut" -> {
+                longdoMap.zoomOut()
             }
             "goCurrentLocation" -> {
                 longdoMap.updateAndShowCurrentLocation()
