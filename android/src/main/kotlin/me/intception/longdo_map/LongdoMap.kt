9cd821def63e956f98f9a74276e31dc23959cdd7
Saran Jenjirawat <registbacktown@gmail.com>
2020-01-31 18:02:29 +0700

    .idea updated

--- android/src/main/kotlin/me/intception/longdo_map/LongdoMap.kt
+++ android/src/main/kotlin/me/intception/longdo_map/LongdoMap.kt
@@ -2,6 +2,7 @@ package me.intception.longdo_map
 
 import android.view.View
 import android.widget.Toast
+import com.longdo.api.Map
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.PluginRegistry
@@ -10,18 +11,18 @@ import com.longdo.api.MapGLSurfaceView
 
 class LongdoMap(private val registrar: PluginRegistry.Registrar, viewId: Int, args: Any?) : PlatformView, MethodChannel.MethodCallHandler {
     private val channel = MethodChannel(registrar.messenger(), "me.intception.longdo_map/$viewId")
-    private val longdoMap = getLongdoMap(registrar)
+    private val mapView = getLongdoMap(registrar)
+    private lateinit var longdoMap: Map
 
     init {
         channel.setMethodCallHandler(this)
     }
 
-    private fun getLongdoMap(registrar: PluginRegistry.Registrar): MapGLSurfaceView {
-        return MapGLSurfaceView(registrar.context())
-    }
+    private fun getLongdoMap(registrar: PluginRegistry.Registrar) =
+         MapGLSurfaceView(registrar.context()).also { it.setListener { longdoMap = it } }
 
     override fun getView(): View {
-        return longdoMap
+        return mapView
     }
 
     override fun dispose() {
@@ -32,6 +33,9 @@ class LongdoMap(private val registrar: PluginRegistry.Registrar, viewId: Int, ar
         when (call.arguments) {
             "doSomething" -> {
                 Toast.makeText(registrar.context(), "Do something", Toast.LENGTH_LONG)
+            }
+            "goCurrentLocation" -> {
+                longdoMap.updateAndShowCurrentLocation()
             }
             else -> result.notImplemented()
         }
